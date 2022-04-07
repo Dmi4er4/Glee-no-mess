d0ee18f8167e11b3f72b0118b3fcf0e1bc7507ed
akharasau <antonkharasau@gmail.com>
2022-04-08 02:00:08 +0300

    Remove merge conflict

--- model/tools/random.cpp
+++ model/tools/random.cpp
@@ -1,12 +1,7 @@
 #include "controller.h"
 #include "random.h"
 
-<<<<<<< Updated upstream
 Random::Random() {}
-=======
-Random::Random() {
-}
->>>>>>> Stashed changes
 
 Random& Random::Instance() {
   static Random instance;
